990e1dba68e17f803d40f428387f8fe7a6da5795
baivab mathema <Baivab>
2020-05-04 18:52:24 -0500

    Account Hw2 Completed

--- src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
+++ src/main/java/com/cybr406/account/configuration/SecurityConfiguration.java
@@ -1,14 +1,23 @@
 package com.cybr406.account.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -32,8 +41,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .mvcMatchers(HttpMethod.GET,"/").permitAll()
-                .mvcMatchers(HttpMethod.GET,"/profiles").permitAll()
+                .mvcMatchers(HttpMethod.GET,"/check-user").hasAnyRole("ADMIN","SERVICE")
+                .mvcMatchers(HttpMethod.GET,"/","/**").permitAll()
+                .mvcMatchers(HttpMethod.POST,"/signup").permitAll()
+                .mvcMatchers(HttpMethod.PATCH,"/profiles/a").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -43,6 +54,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic()
         ;
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+            return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public User.UserBuilder userBuilder(){
+        PasswordEncoder passwordEncoder=passwordEncoder();
+        User.UserBuilder user= User.builder();
+        user.passwordEncoder(passwordEncoder::encode);
+        return user;
     }
 
 }
